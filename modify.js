d21e798f1613ae75c2b6d3d5bf7e363c0fd67d91
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-07-24 17:13:11 -0400

    added email feat for better ux exp

--- modify.js
+++ modify.js
@@ -1,11 +1,11 @@
-const admin = require('firebase-admin');
-const readline = require('readline');
+const admin = require("firebase-admin");
+const readline = require("readline");
 
 // Initialize the Firebase Admin SDK with your service account credentials
-const serviceAccount = require('./adminKey.json');
+const serviceAccount = require("./adminKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 // Function to set custom user claims
@@ -15,22 +15,36 @@ async function setCustomUserClaims(uid, isAdmin) {
     await admin.auth().setCustomUserClaims(uid, { admin: isAdmin });
     console.log(`Custom claims set for user ${uid}`);
   } catch (error) {
-    console.error('Error setting custom claims:', error);
+    console.error("Error setting custom claims:", error);
+  }
+}
+
+// Function to get UID from email
+async function getUidFromEmail(email) {
+  try {
+    const user = await admin.auth().getUserByEmail(email);
+    return user.uid;
+  } catch (error) {
+    console.error("Error retrieving user by email:", error);
+    return null;
   }
 }
 
 // Create an interface to read input from the command line
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-// Prompt the user to enter the admin property and UID
-rl.question('Set admin property (true/false): ', (boolean) => {
-  const isAdmin = boolean.toLowerCase() === 'true'; // Convert to boolean
-  rl.question('Please enter the user UID: ', (userUid) => {
-    // Call the function with the entered UID and admin property
-    setCustomUserClaims(userUid, isAdmin);
+// Prompt the user to enter the admin property and email address
+rl.question("Set admin property (true/false): ", (boolean) => {
+  const isAdmin = boolean.toLowerCase() === "true"; // Convert to boolean
+  rl.question("Please enter the user email: ", async (email) => {
+    const uid = await getUidFromEmail(email);
+    if (uid) {
+      // Call the function with the retrieved UID and admin property
+      await setCustomUserClaims(uid, isAdmin);
+    }
     // Close the readline interface
     rl.close();
   });
